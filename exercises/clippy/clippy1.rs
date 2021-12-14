56a36164d5be1f30b0ac5aef36a49cc446e1f4d4
zjp <jiping_zhou@foxmail.com>
2021-12-15 00:01:40 +0800

    fix clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -9,7 +9,14 @@
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if (y - x).abs() < f64::EPSILON {
+    if (y - x).abs() > f64::EPSILON {
+        // issue:
+        // [clippy1.rs passed without any modifications](https://github.com/rust-lang/rustlings/issues/888)
+        //
+        // BTW 你可以通过
+        // https://github.com/rust-lang/rust-clippy#as-a-rustc-replacement-clippy-driver
+        // 即 `clippy-driver clippy1.rs` 来对单个文件（非 Cargo 项目）检测
+        // if y != x {
         println!("Success!");
     }
 }
